d757960a08e809c356c9a66e20f3ad276fe23882
hao.yu <hao.yu@ushow.media>
2021-11-11 14:13:20 +0800

    build success

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <Foundation/Foundation.h>
+#include "modules/audio_device/mac/audio_device_mac.h"
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    NSLog(@"Hello, World!");
+    NSLog(@"main in");
+
+    auto *audioDeviceMac = new webrtc::AudioDeviceMac();
+    audioDeviceMac->Init();
+
+    NSLog(@"main out");
     return 0;
 }
